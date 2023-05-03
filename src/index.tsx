b80630ae2988a68d77115a4d1212b07bc1b88df4
benjamin <benjaminharringtonrose@gmail.com>
2023-05-03 08:35:49 -0400

    chore: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+import React from 'react';
+import { View } from 'react-native';
+
+export const ControlKnob = () => {
+  return <View />;
+};
